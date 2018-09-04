2435aa49ed1402e045ff472aa379ba054a53e172
Josiane <josianeyav@yahoo.fr>
2018-09-04 18:08:02 +0200

    Ajouté une meilleur page d'erreur

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Erreur</title>
+</head>
+<body>
+    <h1 style="color: red; text-align: center; margin-top: 200px">
+        Vous n'avez pas l'authorisation d'effectuer cette action.
+    </h1>
+</body>
+</html>
